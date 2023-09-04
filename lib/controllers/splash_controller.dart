247a758e8b92e326e5efb673a97624031fce98f9
MK <103623651+MangeshKakade@users.noreply.github.com>
2023-09-04 16:14:52 +0530

    Update

--- lib/controllers/splash_controller.dart
+++ lib/controllers/splash_controller.dart
@@ -2,7 +2,7 @@ import 'package:get/get.dart';
 
 class SplashController extends GetxController {
   void init() {
-    Future.delayed(Duration(seconds: 25), () {
+    Future.delayed(Duration(seconds: 3), () {
       Get.offAllNamed('/slider');
     });
   }
